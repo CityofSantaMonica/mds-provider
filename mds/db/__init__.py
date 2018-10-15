b5a2461ad81d18d012699d17aee9a99ad42de989
Kegan Maher <kegan.maher@smgov.net>
2018-10-15 00:09:41 -0700

    refactor to support loading data directly
    i.e. without having to read it from a json file

--- mds/db/__init__.py
+++ mds/db/__init__.py
@@ -3,5 +3,5 @@ Work with MDS Provider data.
 """
 
 from mds.db import db
-from mds.db.db import data_engine, read_data_file, load_status_changes, load_trips
+from mds.db.db import data_engine, load_status_changes, load_trips
 
--- mds/db/db.py
+++ mds/db/db.py
@@ -6,6 +6,7 @@ import json
 import mds
 from mds.db import sql
 from mds.json import read_data_file
+import os
 import pandas as pd
 import psycopg2
 import sqlalchemy
@@ -22,6 +23,24 @@ def data_engine(user, password, db, host, port):
     url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
     return sqlalchemy.create_engine(url)
 
+def load_from_df(df, record_type, engine, table, before_load=None, stage_first=True):
+    # run any pre-processors
+    if before_load is not None:
+        before_load(df)
+
+    # insert this DataFrame into a fresh table
+    df.to_sql(table, engine, if_exists="replace", index=False)
+
+    # now insert from the temp table to the actual table
+    if stage_first:
+        with engine.begin() as conn:
+            if record_type == mds.STATUS_CHANGES:
+                query = sql.insert_status_changes_from(table)
+            elif record_type == mds.TRIPS:
+                query = sql.insert_trips_from(table)
+            if query is not None:
+                conn.execute(query)
+
 def load_from_file(src, record_type, engine, table, before_load=None):
     """
     Load the data file of type :record_type: at :src: in the table :table: using the connection
@@ -32,22 +51,39 @@ def load_from_file(src, record_type, engine, table, before_load=None):
     """
     # read the data file
     _, df = read_data_file(src, record_type)
+    load_from_df(df, record_type, engine, table, before_load=before_load)
 
-    # run any pre-processors
-    if before_load is not None:
-        before_load(df)
+def load_from_records(data, record_type, engine, table, before_load=None):
+    """
+    Load the dict :data: of type :record_type: into the table :table: using the connection defined by :engine:.
 
-    # insert this DataFrame into a new temporary table
-    df.to_sql(table, engine, if_exists="replace", index=False)
+    :before_load: is an optional callback to pre-process a DataFrame before loading
+    it into :table:.
+    """
+    df = pd.DataFrame.from_records(data)
+    load_from_df(df, record_type, engine, table, before_load=before_load)
 
-    # now insert from the temp table to the actual table
-    with engine.begin() as conn:
-        if record_type == mds.STATUS_CHANGES:
-            query = sql.insert_status_changes_from(table)
-        elif record_type == mds.TRIPS:
-            query = sql.insert_trips_from(table)
-        if query is not None:
-            conn.execute(query)
+def load_from_sources(sources, record_type, engine, table, before_load=None):
+    """
+    Load from one or more file path or dict sources.
+    """
+    for src in sources:
+        if isinstance(src, str) and os.path.exists(src):
+            load_from_file(
+                src=src,
+                record_type=record_type,
+                engine=engine,
+                table=table,
+                before_load=before_load)
+        elif isinstance(sources, dict) or isinstance(sources, list):
+            load_from_records(
+                data=sources[src],
+                record_type=record_type,
+                engine=engine,
+                table=table,
+                before_load=before_load)
+        else:
+            print("Couldn't understand source: {}. Skipping.".format(src))
 
 def _json_cols_tostring(df, cols):
     """
@@ -63,13 +99,7 @@ def load_status_changes(sources, engine):
     json_cols = ["event_location"]
     before_load = lambda df: _json_cols_tostring(df, json_cols)
 
-    for src in sources:
-        load_from_file(
-            src=src,
-            record_type=mds.STATUS_CHANGES,
-            engine=engine,
-            table=TEMP_SC,
-            before_load=before_load)
+    load_from_sources(sources, mds.STATUS_CHANGES, engine, TEMP_SC, before_load=before_load)
 
 def load_trips(sources, engine):
     """
@@ -78,11 +108,5 @@ def load_trips(sources, engine):
     json_cols = ["route"]
     before_load = lambda df: _json_cols_tostring(df, json_cols)
 
-    for src in sources:
-        load_from_file(
-            src=src,
-            record_type=mds.TRIPS,
-            engine=engine,
-            table=TEMP_TRIPS,
-            before_load=before_load)
+    load_from_sources(sources, mds.TRIPS, engine, TEMP_TRIPS, before_load=before_load)
 
