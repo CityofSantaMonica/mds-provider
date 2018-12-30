8bb4ea320307da3736766d026fd0385ebc3529da
Kegan Maher <kegan.maher@smgov.net>
2018-12-29 23:09:18 -0800

    fixing ON CONFLICT syntax
    need to specify the conflicting columns
    there should be a corresponding constraint on these columns
    (e.g. primary key, unique constraint)

--- mds/db/sql.py
+++ mds/db/sql.py
@@ -5,20 +5,20 @@ SQL scripts for MDS Provider database CRUD.
 import mds
 
 
+__conflict_nothing = "ON CONFLICT DO NOTHING"
+
+
 def insert_status_changes_from(source_table, dest_table=mds.STATUS_CHANGES, on_conflict_update=False):
     """
-    Generate an INSERT INTO statement from :source_table: to the Status Changes table, that
-    ignores records that conflict based on existing uniqueness constraints.
+    Generate an INSERT INTO statement from :source_table: to the Status Changes table.
     """
-    conflict_update = """ON CONFLICT DO UPDATE SET
-    event_type = EXCLUDED.event_type::event_types,
-    event_type_reason = EXCLUDED.event_type_reason::event_type_reasons,
-    event_location = EXCLUDED.event_location::json,
-    battery_pct = EXCLUDED.battery_pct,
-    associated_trips = EXCLUDED.associated_trips::uuid[]
+    conflict_update = """ON CONFLICT (provider_id, device_id, event_time) DO UPDATE SET
+        event_type = cast(EXCLUDED.event_type as event_types),
+        event_type_reason = cast(EXCLUDED.event_type_reason as event_type_reasons),
+        event_location = cast(EXCLUDED.event_location as json),
+        battery_pct = EXCLUDED.battery_pct,
+        associated_trips = cast(EXCLUDED.associated_trips as uuid[])
     """
-
-    conflict_nothing = "ON CONFLICT DO NOTHING"
 
     return f"""
     INSERT INTO "{dest_table}"
@@ -37,41 +37,38 @@ def insert_status_changes_from(source_table, dest_table=mds.STATUS_CHANGES, on_c
         associated_trips
     )
     SELECT
-        provider_id::uuid,
+        cast(provider_id as uuid),
         provider_name,
-        device_id::uuid,
+        cast(device_id as uuid),
         vehicle_id,
-        vehicle_type::vehicle_types,
-        propulsion_type::propulsion_types[],
-        event_type::event_types,
-        event_type_reason::event_type_reasons,
-        to_timestamp(event_time) AT TIME ZONE 'UTC',
-        event_location::json,
+        cast(vehicle_type as vehicle_types),
+        cast(propulsion_type as propulsion_types[]),
+        cast(event_type as event_types),
+        cast(event_type_reason as event_type_reasons),
+        to_timestamp(event_time) at time zone 'UTC',
+        cast(event_location as json),
         battery_pct,
-        associated_trips::uuid[]
+        cast(associated_trips as uuid[])
     FROM "{source_table}"
-    { conflict_update if on_conflict_update else conflict_nothing }
+    { conflict_update if on_conflict_update else __conflict_nothing }
     ;
     """
 
 def insert_trips_from(source_table, dest_table=mds.TRIPS, on_conflict_update=False):
     """
-    Generate an INSERT INTO statement from :source_table: to the Trips table, that
-    ignores records that conflict based on existing uniqueness constraints.
+    Generate an INSERT INTO statement from :source_table: to the Trips table.
     """
-    conflict_update = """ON CONFLICT DO UPDATE SET
+    conflict_update = """ON CONFLICT (provider_id, trip_id) DO UPDATE SET
         trip_duration = EXCLUDED.trip_duration,
         trip_distance = EXCLUDED.trip_distance,
-        route = EXCLUDED.route::json,
+        route = cast(EXCLUDED.route as json),
         accuracy = EXCLUDED.accuracy,
-        start_time = to_timestamp(EXCLUDED.start_time) AT TIME ZONE 'UTC',
-        end_time = to_timestamp(EXCLUDED.end_time) AT TIME ZONE 'UTC',
+        start_time = to_timestamp(EXCLUDED.start_time) at time zone 'UTC',
+        end_time = to_timestamp(EXCLUDED.end_time) at time zone 'UTC',
         parking_verification_url = EXCLUDED.parking_verification_url,
         standard_cost = EXCLUDED.standard_cost,
         actual_cost = EXCLUDED.actual_cost
     """
-
-    conflict_nothing = "ON CONFLICT DO NOTHING"
 
     return f"""
     INSERT INTO "{dest_table}"
@@ -94,24 +91,24 @@ def insert_trips_from(source_table, dest_table=mds.TRIPS, on_conflict_update=Fal
         actual_cost
     )
     SELECT
-        provider_id::uuid,
+        cast(provider_id as uuid),
         provider_name,
-        device_id::uuid,
+        cast(device_id as uuid),
         vehicle_id,
-        vehicle_type::vehicle_types,
-        propulsion_type::propulsion_types[],
-        trip_id::uuid,
+        cast(vehicle_type as vehicle_types),
+        cast(propulsion_type as propulsion_types[]),
+        cast(trip_id as uuid),
         trip_duration,
         trip_distance,
-        route::json,
+        cast(route as json),
         accuracy,
-        to_timestamp(start_time) AT TIME ZONE 'UTC',
-        to_timestamp(end_time) AT TIME ZONE 'UTC',
+        to_timestamp(start_time) at time zone 'UTC',
+        to_timestamp(end_time) at time zone 'UTC',
         parking_verification_url,
         standard_cost,
         actual_cost
     FROM "{source_table}"
-    { conflict_update if on_conflict_update else conflict_nothing }
+    { conflict_update if on_conflict_update else __conflict_nothing }
     ;
     """
 
