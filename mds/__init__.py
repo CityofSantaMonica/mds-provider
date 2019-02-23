954570c55abd99b74b4226837d544d166e382ca3
Kegan Maher <kegan.maher@smgov.net>
2019-02-22 17:37:22 -0800

    creating mds.version module
    mds.version.Version helps parse and compare version strings

--- mds/__init__.py
+++ mds/__init__.py
@@ -2,23 +2,6 @@
 Tools for working with Mobility Data Specification Provider data.
 """
 
-from mds._version import __mds_version__, __version__
-
-
 STATUS_CHANGES = "status_changes"
 
 TRIPS = "trips"
-
-
-def MDS_VERSION():
-    """
-    :returns: The minimum MDS version supported by this version of `mds-provider`.
-    """
-    return __mds_version__
-
-
-def VERSION():
-    """
-    :returns: The version of `mds-provider` currently being used.
-    """
-    return __version__
