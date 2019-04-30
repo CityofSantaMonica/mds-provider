bf5c0ec90f4206bac046fab3b81f4be6babe6cc6
Kegan Maher <kegan.maher@smgov.net>
2019-04-30 11:11:03 -0700

    more Provider class UX improvements
    * refactor init to handle parsing and searching the registry
    * simplify configuration via init
    * get_registry is now a classmethod

--- mds/providers.py
+++ mds/providers.py
@@ -15,23 +15,90 @@ class Provider():
     """
     A simple model for an entry in the Provider registry.
     """
-    def __init__(self, *args, **kwargs):
-        self.provider_name = kwargs.pop("provider_name", None)
+    __registry = {}
 
-        provider_id = kwargs.pop("provider_id", None)
-        self.provider_id = provider_id if isinstance(provider_id, UUID) else UUID(provider_id)
+    def __init__(self, identifier=None, config={}, ref=DEFAULT_REF, path=None, **kwargs):
+        """
+        Initialize a new Provider instance.
 
-        self.url = self._clean_url(kwargs.pop("url", None))
-        self.mds_api_url = self._clean_url(kwargs.pop("mds_api_url", None))
-        self.gbfs_api_url = self._clean_url(kwargs.pop("gbfs_api_url", None))
+        Interrogate the Provider registry or a local file path, using the given identifier
+        (provider_id or provider_name).
 
-        for k,v in kwargs.items():
-            setattr(self, k, v)
+        Optionally pass a configuration dict to merge with the Provider instance.
+        """
+        # parsing a Provider record
+        if not identifier:
+            self.provider_name = kwargs.pop("provider_name", None)
+
+            provider_id = kwargs.pop("provider_id", None)
+            self.provider_id = provider_id if isinstance(provider_id, UUID) else UUID(provider_id)
+
+            self.url = self.__clean_url(kwargs.pop("url", None))
+            self.mds_api_url = self.__clean_url(kwargs.pop("mds_api_url", None))
+            self.gbfs_api_url = self.__clean_url(kwargs.pop("gbfs_api_url", None))
+
+            for k,v in kwargs.items():
+                setattr(self, k, v)
+
+        # interrogate the registry
+        else:
+            try:
+                identifier = UUID(identifier)
+            except ValueError:
+                pass
+
+            # obtain the registry
+            registry = Provider.get_registry(ref=ref, path=path)
+
+            # filter for matching provider(s)
+            registry = [p for p in registry if any([
+                isinstance(identifier, str) and p.provider_name.lower() == identifier.lower(),
+                isinstance(identifier, UUID) and p.provider_id == identifier
+            ])]
+
+            if len(registry) == 1:
+                # re-init with the record from registry and config
+                _kwargs = { **vars(registry[0]), **(config or {}) }
+                Provider.__init__(self, **_kwargs)
+            else:
+                raise ValueError(f"Can not obtain a single provider matching '{identifier}'.")
 
     def __repr__(self):
-        return f"<Provider name:'{self.provider_name}' api_url:'{self.mds_api_url}' id:'{str(self.provider_id)}'>"
+        return f"<Provider provider_name:'{self.provider_name}' provider_id:'{str(self.provider_id)}' mds_api_url:'{self.mds_api_url}'>"
 
-    def _clean_url(self, url):
+    @classmethod
+    def get_registry(cls, ref=DEFAULT_REF, path=None):
+        """
+        Parse a Provider registry file into a list of Provider instances.
+
+        By default, download the official registry from GitHub `master`.
+
+        Optionally download from the specified :ref:, which could be any of:
+            - git branch name
+            - commit hash (long or short)
+            - git tag
+
+        Or use the :path: kwarg to skip the download and parse a local registry file.
+        """
+        def __get():
+            if path:
+                with open(path, "r") as f:
+                    return cls.__parse_csv(f.readlines())
+            else:
+                url = PROVIDER_REGISTRY.format(ref or DEFAULT_REF)
+                with requests.get(url, stream=True) as r:
+                    lines = (line.decode("utf-8").replace(", ", ",") for line in r.iter_lines())
+                    return cls.__parse_csv(lines)
+
+        # get/cache this registry reference
+        key = (ref, path)
+        if key not in cls.__registry:
+            cls.__registry[key] = __get()
+
+        return cls.__registry[key]
+
+    @staticmethod
+    def __clean_url(url):
         """
         Helper to return a normalized URL
         """
@@ -41,65 +108,9 @@ class Provider():
         else:
             return None
 
-    def configure(self, config):
+    @staticmethod
+    def __parse_csv(lines):
         """
-        Merge attributes from :config: into this Provider instance.
+        Helper parses CSV lines into a list of Provider instances.
         """
-        _kwargs = { **vars(self), **(config or {}) }
-        Provider.__init__(self, **_kwargs)
-        return self
-
-    @classmethod
-    def Get(cls, provider, config=None, ref=DEFAULT_REF, file=None):
-        """
-        Obtain a Provider instance from the registry or a local file, using the given identifier
-        (provider_id or provider_name).
-
-        :config: A dict of attributes to merge with the Provider instance.
-        """
-        try:
-            provider = UUID(provider)
-        except ValueError:
-            pass
-
-        def __filter(data, target):
-            return any([
-                isinstance(data, str) and target.provider_name.lower() == data.lower(),
-                isinstance(data, UUID) and target.provider_id == data
-            ])
-
-        providers = [p for p in get_registry(ref=ref, file=file) if __filter(provider, p)]
-
-        if len(providers) == 1:
-            return providers[0].configure(config) if config else providers[0]
-        else:
-            raise ValueError(f"Can not obtain a single provider matching '{provider}'.")
-
-
-def get_registry(ref=DEFAULT_REF, file=None):
-    """
-    Parse a Provider registry file; by default, download the official registry from GitHub `master`.
-
-    Optionally download from the specified :ref:, which could be any of:
-        - git branch name
-        - commit hash (long or short)
-        - git tag
-
-    Or use the :file: kwarg to skip the download and parse a local registry file.
-    """
-    providers = []
-
-    def __parse(lines):
-        for record in csv.DictReader(lines):
-            providers.append(Provider(**record))
-
-    if file:
-        with open(file, "r") as f:
-            __parse(f.readlines())
-    else:
-        url = PROVIDER_REGISTRY.format(ref or DEFAULT_REF)
-        with requests.get(url, stream=True) as r:
-            lines = (line.decode("utf-8").replace(", ", ",") for line in r.iter_lines())
-            __parse(lines)
-
-    return providers
+        return [Provider(**record) for record in csv.DictReader(lines)]
