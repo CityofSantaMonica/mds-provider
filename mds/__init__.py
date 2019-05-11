d1aa292fb90f37438627cd0851b8a21a5f195371
Kegan Maher <kegan.maher@smgov.net>
2019-05-11 15:58:45 -0700

    all the naming updates

--- mds/__init__.py
+++ mds/__init__.py
@@ -2,9 +2,9 @@
 Tools for working with Mobility Data Specification Provider data.
 """
 
-from .api import ProviderClient
-from .db import ProviderDatabase, data_engine
-from .files import ProviderDataFiles
+from .api import Client
+from .db import Database, data_engine
+from .files import ConfigFile, DataFile
 from .providers import Provider
-from .schemas import STATUS_CHANGES, TRIPS, ProviderDataValidator
+from .schemas import STATUS_CHANGES, TRIPS, DataValidator
 from .versions import Version
